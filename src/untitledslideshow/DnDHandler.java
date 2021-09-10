c010fe91933ebc429361a6930cb46db9fcff94b6
Your Name <annaleisebk@outlook.com>
2021-09-10 12:36:27 -0500

    improve dnd comments

--- src/untitledslideshow/DnDHandler.java
+++ src/untitledslideshow/DnDHandler.java
@@ -28,6 +28,8 @@ public class DnDHandler {
     /**
      * Set the start mouse x/y.
      * If their coordinates are within a reel, call removeByPosition on that reel, and set the dragItem to the return.
+     * Else if their coordinates are within the inspector, use inspector.getFocusItem(), and set the dragItem to the return.
+     * Else if their coordinates are within a palette, use positionToItem on appropriate palette, and set the dragItem to the return.
      * Change the cursor.
      */
     public void startDrag()
